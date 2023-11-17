507ed2f3ed4d80a7223bdc6ab97a5948d682147e
dubidubob <sildredel@naver.com>
2023-11-17 12:29:43 +0900

    login file add

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ application = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@application.route("/login")
+def view_login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=5000)
